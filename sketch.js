91af310bd03687c7d7342813bfb72731eb8659e8
R2mix <11405001+R2mix@users.noreply.github.com>
2023-06-24 22:59:15 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,23 +6,35 @@
 let imgHair, imgEyes, imgPrenose, imgNose, imgMouth, imgArmor, imgHelm;
 let rngNose, rngMouth, rngEye, rngHair, rngPrenose, rngArmor, rngHelm;
 let imgSave, imgHide, saving = false, savingCounter = 0;
-let hideHair = false, hideEyes = false, hideNose = false, hidePrenose = false, hideMouth = false, hideArmor = false, hideHelm = false;
+let hideHair = false, hideEyes = false, hideNose = false, hidePrenose = false, hideMouth = false, hideArmor = false, hideHelm = true;
 let selected = "";
-let hairX = 0, hairY = 0, eyeX = 0, eyeY = 100, prenoseX = 0, prenoseY = 250, noseX = 0, noseY = 250, mouthX = 0, mouthY = 300, armorX = 0, armorY = 400, helmX = 0, helmY = 0;
-let hairSizeW = 1024,hairSizeH = 1024, eyesSizeW = 256,eyesSizeH = 256, prenoseSizeW = 512, prenoseSizeH = 512;
-let noseSizeW = 512, noseSizeH = 512, mouthSizeW = 512, mouthSizeH = 512, armorSizeW = 1024, armorSizeH = 1024, helmSizeW = 1024, helmSizeH = 1024;
+let hairX = 100, hairY = 0, eyeX = 250, eyeY = 300, prenoseX = 380, prenoseY = 365, noseX = 350, noseY = 380, mouthX = 333, mouthY = 510, armorX = -350, armorY = 760, helmX = 0, helmY = 0;
+let hairSizeW = 600, hairSizeH = 600, eyesSizeW = 300, eyesSizeH = 300, prenoseSizeW = 60, prenoseSizeH = 60;
+let noseSizeW = 100, noseSizeH = 100, mouthSizeW = 135, mouthSizeH = 135, armorSizeW = 1600, armorSizeH = 1600, helmSizeW = 1024, helmSizeH = 1024;
 
+
+function windowResized() {
+resizeCanvas(windowWidth, windowHeight);
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rngNose = int(random(41));
-  rngMouth = int(random(45));
-  rngPrenose = int(random(28));
-  rngEye = int(random(63));
-  rngHair = int(random(114));
-  rngArmor = int(random(108));
-  rngHelm = int(random(26));
-
+  rngNose = 0;
+  rngMouth = 4;
+  rngPrenose = 1;
+  rngEye = 34;
+  rngHair = 35;
+  rngArmor = 14;
+  rngHelm = 0;
+  /*
+    rngNose = int(random(41));
+    rngMouth = int(random(45));
+    rngPrenose = int(random(28));
+    rngEye = int(random(63));
+    rngHair = int(random(114));
+    rngArmor = int(random(108));
+    rngHelm = int(random(26));
+    */
   imgHair = loadImage("data/hair/hair_" + rngHair + ".png");
   imgEyes = loadImage("data/eyes/eyes_" + rngEye + ".png");
   imgPrenose = loadImage("data/prenose/prenose_" + rngPrenose + ".png");
@@ -32,6 +44,7 @@ function setup() {
   imgHelm = loadImage("data/helmet/helm_" + rngHelm + ".png");
   imgSave = loadImage("save.png");
   imgHide = loadImage("hide.png");
+
 }
 
 function draw() {
@@ -40,8 +53,9 @@ function draw() {
   fill(0)
   push();
   noStroke();
-  text('Fantasy chara Designer', 40 ,40);
+  text('Fantasy chara Designer', 40, 40);
   pop();
+  imageMode(CORNER)
   personnage();
   saves();
   if (saving == false) { allButton(); }
@@ -55,15 +69,15 @@ function allButton() {
   buttonPP();
   buttonImageSelected(40 + 80, imgEyes, "eye", rngEye);
   buttonNext(40 + 80);
-  buttonImageSelected(40 +80 * 2, imgPrenose, "prenose", rngPrenose);
+  buttonImageSelected(40 + 80 * 2, imgPrenose, "prenose", rngPrenose);
   buttonNext(40 + 80 * 2);
-  buttonImageSelected(40 +80 * 3, imgNose, "nose", rngNose);
+  buttonImageSelected(40 + 80 * 3, imgNose, "nose", rngNose);
   buttonNext(40 + 80 * 3);
-  buttonImageSelected(40 +80 * 4, imgMouth, "mouth", rngMouth);
+  buttonImageSelected(40 + 80 * 4, imgMouth, "mouth", rngMouth);
   buttonNext(40 + 80 * 4);
-  buttonImageSelected(40 +80 * 5, imgHair, "hair", rngHair);
+  buttonImageSelected(40 + 80 * 5, imgHair, "hair", rngHair);
   buttonNext(40 + 80 * 5);
-  buttonImageSelected(40 +80 * 6, imgArmor, "armor", rngArmor);
+  buttonImageSelected(40 + 80 * 6, imgArmor, "armor", rngArmor);
   buttonNext(40 + 80 * 6);
   buttonImageSelected(40 + 80 * 7, imgHelm, "helm", rngHelm);
   buttonNext(40 + 80 * 7);
@@ -74,7 +88,6 @@ function saves() {
   if (saving == true) {
     save('myCharacter.png');
     saving = false;
-
   }
 }
 
@@ -107,43 +120,43 @@ function personnage() {
     eyeX = eyeX + (mouseX - pmouseX);
     eyeY = eyeY + (mouseY - pmouseY);
   }
-  if (hideEyes == false) { image(imgEyes, eyeX, eyeY, eyesSizeW, eyesSizeH); }
+  if (hideEyes == false) { image(imgEyes, eyeX, eyeY, eyesSizeW, imgEyes.height * eyesSizeH / imgEyes.width); }
 
   if (buttonIsClicked(prenoseX, prenoseY, prenoseSizeW, prenoseSizeH) && selected == "prenose") {
     prenoseX = prenoseX + (mouseX - pmouseX);
     prenoseY = prenoseY + (mouseY - pmouseY);
   }
-  if (hidePrenose == false) { image(imgPrenose, prenoseX, prenoseY, prenoseSizeW, prenoseSizeH); }
+  if (hidePrenose == false) { image(imgPrenose, prenoseX, prenoseY, prenoseSizeW, imgPrenose.height * prenoseSizeH / imgPrenose.width); }
 
   if (buttonIsClicked(noseX, noseY, noseSizeW, noseSizeH) && selected == "nose") {
     noseX = noseX + (mouseX - pmouseX);
     noseY = noseY + (mouseY - pmouseY);
   }
-  if (hideNose == false) { image(imgNose, noseX, noseY, noseSizeW, noseSizeH); }
+  if (hideNose == false) { image(imgNose, noseX, noseY, noseSizeW, imgNose.height * noseSizeH / imgNose.width); }
 
   if (buttonIsClicked(mouthX, mouthY, mouthSizeW, mouthSizeH) && selected == "mouth") {
     mouthX = mouthX + (mouseX - pmouseX);
     mouthY = mouthY + (mouseY - pmouseY);
   }
-  if (hideMouth == false) { image(imgMouth, mouthX, mouthY, mouthSizeW, mouthSizeH); }
+  if (hideMouth == false) { image(imgMouth, mouthX, mouthY, mouthSizeW, imgMouth.height * mouthSizeH / imgMouth.width); }
 
   if (buttonIsClicked(hairX, hairY, hairSizeW, hairSizeH) && selected == "hair") {
     hairX = hairX + (mouseX - pmouseX);
     hairY = hairY + (mouseY - pmouseY);
   }
-  if (hideHair == false) { image(imgHair, hairX, hairY, hairSizeW, hairSizeH); }
-  
+  if (hideHair == false) { image(imgHair, hairX, hairY, hairSizeW, imgHair.height * hairSizeH / imgHair.width); }
+
   if (buttonIsClicked(armorX, armorY, armorSizeW, armorSizeH) && selected == "armor") {
     armorX = armorX + (mouseX - pmouseX);
     armorY = armorY + (mouseY - pmouseY);
   }
-  if (hideArmor == false) { image(imgArmor, armorX, armorY, armorSizeW, armorSizeH); }
+  if (hideArmor == false) { image(imgArmor, armorX, armorY, armorSizeW, imgArmor.height * armorSizeH / imgArmor.width); }
 
   if (buttonIsClicked(helmX, helmY, helmSizeW, helmSizeH) && selected == "helm") {
     helmX = helmX + (mouseX - pmouseX);
     helmY = helmY + (mouseY - pmouseY);
   }
-  if (hideHelm == false) { image(imgHelm, helmX, helmY, helmSizeW, helmSizeH); }
+  if (hideHelm == false) { image(imgHelm, helmX, helmY, helmSizeW, imgHelm.height * helmSizeH / imgHelm.width); }
 
 }
 
@@ -231,7 +244,7 @@ function mousePressed() {
   if (buttonIsClicked(width - 160, 40 + 60, 80, 80)) {
     selected = "eye";
   }
-  if (buttonIsClicked(width - 160, 40 +140, 80, 80)) {
+  if (buttonIsClicked(width - 160, 40 + 140, 80, 80)) {
     selected = "prenose";
   }
   if (buttonIsClicked(width - 160, 40 + 220, 80, 80)) {
@@ -406,7 +419,7 @@ function buttonPP() {
   rect(width - 120, 20, 40, 40);
   line(width - 120 + 3, 30 + 10, width - 120 + 40 - 3, 30 + 10);
   pop();
-  
+
   // RATIO H
   push();
   if (buttonIsClicked(width - 120, 60, 40, 40)) {
